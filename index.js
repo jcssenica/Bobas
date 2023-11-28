e7a073511a5ad49661a953d33733e2ed3f92a94c
Yuming Jessica Chen <jcssenica@gmail.com>
2023-11-27 19:04:14 -0500

    add all

--- index.js
+++ index.js
@@ -1,125 +1,135 @@
-import express from 'express'
+import express from 'express';
 
-// import jabber from 'jabber'
+const app = express();
 
-import randomWords from "random-words";
+const port = process.env.PORT || 3001;
 
-//import package
-// import isVegan from 'is-vegan';
-//specify a port using
-const port = process.env.PORT || 3001
-
-//import a package/library
-const app = express()
-// var word = randomWords()
-// const isVeganIngredient = isVegan()
-// const check = checkIngredients()
-
-// console.log(
-//     isVegan.isVeganIngredient('soy') // true // false
-// )
-// console.log(
-//     isVegan.isVeganIngredientList(['aspic', 'albumin'])
-// )
-// const jabberInstance = new Jabber();
-// const Jabber = require("jabber");
-// const randonWords = require('random-words');
-
-
-//app = express [line 4]
 app.use(express.json())
 
-// let cars = {
-//     //[1]=ID
-//     1: {
-//      name: "Car 1", 
-//      color: "Red", 
-//      brand: "Nissan", 
-//      year: "2000"
-//     },
-//     2: {
-//      name: "Car 2", 
-//      color: "Blue", 
-//      brand: "Audi", 
-//      year: "1995"
-//     },
-//     3: {
-//      name: "Car 3", 
-//      color: "Green",
-//      brand: "Toyota", 
-//      year: "2005"
-//     },
-//     4: {
-//      name: "Car 4",
-//      color: "Green", 
-//      brand: "Hyundai",
-//      year: "2010"
-//     }
-//  }
+let milkTeas = {
+    1: {
+        name: "Earl Grey Milk Tea",
+        ingredients: ['black tea', 'milk', 'boba'],
+    },
+    2: {
+        name: "Jasmine Milk Tea",
+        ingredients: ['jasmin tea', 'milk', 'boba'],
+    },
+    3: {
+        name: "Trio Milk Tea",
+        ingredients: ['black rtea', 'milk', 'boba', 'grass jelly', 'pudding'],
+    },
+    4: {
+        name: "Oreo Garden Milk Tea",
+        ingredients: ['earl gray tea', 'milk', 'oreo cookie crumbs'],
+    },
+    5: {
+        name: "Oolong Milk Tea",
+        ingredients: ['oolong tea', 'milk'],
+    },
+    6: {
+        name: "Thai Milk Tea",
+        ingredients: ['thai ice tea', 'condensed milk', 'cane sugar'],
+    },
+}
 
- //query structures use?
- // car-api.com?color=Green [client send in request in this format]
- //('/',route, function
- //[content passed in] =>
-//  app.get('/',(req,res) => {
-//     let matchingCars =[];
+// app.get('/', (req,res) => {
+//     const userIngredient = req.query.ingredient;
+//     let matchingMilkTeas = [];
 
-//     //funtion for each object
-//     //object--key = ID number--value of each key
-//     Object.keys(cars).forEach((key,value) => {
-//         //give me the value entered, give me the value of 
-//         //ID number object, compare and check if match the 
-//         //value entered
-//         if(req.query.color == cars[key].color){
-//             //Only give/push ID back to the user
-//             matchingCars.push(key)
+//     Object.keys(milkTeas).forEach((key,value) => {
+//         if(req.query.ingredient == milkTeas[key].ingredient){
+//             matchingMilkTeas.push(key)
 //         }
 //     })
-//     //after doing through the array, send it back to the user
-//     res.send(matchingCars)
-//  })
-
-// app.get('/check-vegan-ingredient/:ingredient', (req,res) => {
-//     const ingredient = req.params.ingredient;
+//     res.send(matchingMilkTeas)
 // })
 
-//url parameter use / 
-//car-api.com/car/:1
-app.get('/car/:car',(req, res) => {
-    //for user spending back the car
-    let car;
-    //:1 only the value after:
-    Object.keys(cars).forEach((key,value) => {
-        //user input in url
-        if(req.params.car.substring(1) == key){
-            car = cars[key]
+app.get('/', (req,res) => {
+    const userIngredient = req.query.ingredient;
+
+    if (!userIngredient) {
+        return res.status(400).send('Please provide an ingredient in the URL.');
+    }
+
+    let matchingMilkTeas = [];
+    
+    Object.keys(milkTeas).forEach((key) => {
+        const teaIngredients = milkTeas[key].ingredients;
+
+        if(Array.isArray(teaIngredients) && teaIngredients.includes(userIngredient)) {
+            matchingMilkTeas.push({
+                id: key,
+                name: milkTeas[key].name,
+                ingredients: teaIngredients,
+            });
         }
     })
-    //sending back the variable
-    res.send(car)
-
-})
-
-app.get('/word6', (req,res) => {
-    const sixLetterWord = randomWords({exactly:1, maxLength:6})[0];
-    res.send(`Generated 6-letter word: ${sixLetterWord}`)
+    res.send(matchingMilkTeas)
 })
 
 
-// app.get('/is-vegan/:ingredient', (req, res) => {
-//     const ingredient = req.params.ingredient.toLowerCase(); // Convert to lowercase for case-insensitive matching
-//     const isVeganIngredient = isVegan();
-  
-//     if (!ingredient) {
-//         return res.status(400).send('Please provide an ingredient in the URL parameter.');
-//       }
+app.get('/milkTea/:milkTea', (req,res) => {
+    const requestedMilkTeaName = req.params.milkTea;
+
+    let matchingMilkTea = null;
     
-//       const isVeganResult = isVegan.isVeganIngredient(ingredient);
-//       const responseMessage = `${ingredient} is ${isVeganResult ? 'vegan' : 'not vegan'}.`;
-      
-//       res.send(responseMessage);
-//   });
+    Object.keys(milkTeas).forEach((key) => {
+        const currentMilkTea = milkTeas[key];
+        if(currentMilkTea.name.toLowerCase() === requestedMilkTeaName.toLowerCase()) {
+            matchingMilkTea = currentMilkTea;
+        }
+    })
+    res.send(matchingMilkTea);
+})
 
- app.listen(port,() => {
-    console.log(`App listening on ${port}`)
- })
+// let milkTeas = [
+//     { name: 'Earl Grey Milk Tea', ingredient: ['black tea', 'milk', 'boba'] },
+//     { name: 'Jasmine Milk Tea', ingredient: ['jasmin tea', 'milk', 'boba'] },
+//     { name: 'Trio Milk Tea', ingredient: ['black rtea', 'milk', "boba", "grass jelly", "pudding"] },
+//     { name: 'Oreo Garden Milk Tea', ingredient: ['earl gray tea', 'milk', 'oreo cookie crumbs'] },
+//     { name: 'Oolong Milk Tea', ingredient: ['oolong tea', 'milk'] },
+//     { name: 'Thai Milk Tea', ingredient: ['thai ice tea', 'condensed milk', 'cane sugar'] },
+   
+// ];
+
+
+// app.get('/milkTeaByIngredient', (req, res) => {
+//     const craving = req.query.ingredient;
+//     let matches = [];
+
+//     for (let milkTea of milkTeas) {
+//         if (milkTea.ingredient.includes(craving.toLowerCase())) {
+//             matches.push(milkTea);
+//         }
+//     }
+
+//     if (matches.length === 0) {
+//         return res.status(404).send('No Milk Tea found with that ingredient.');
+//     }
+
+//     res.send(matches);
+// });
+
+// // 根据颜色筛选水晶
+// app.get('/crystalsByColor', (req, res) => {
+//     const desiredColor = req.query.color;
+//     let matches = [];
+
+//     for (let crystal of crystals) {
+//         if (crystal.color.toLowerCase() === desiredColor.toLowerCase()) {
+//             matches.push(crystal);
+//         }
+//     }
+    
+//     if (matches.length === 0) {
+//         return res.status(404).send('No crystals found with that color.');
+//     }
+
+//     res.send(matches);
+// });
+
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
