babcf679f866a2d62bedf6eabe64b55475ccf761
Yuming Jessica Chen <jcssenica@gmail.com>
2023-12-04 14:43:51 -0500

    npm install cors

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 const port = process.env.PORT || 3001;
 
 app.use(express.json())
+app.use(cors({
+    origin: '*'
+}))
 
 let milkTeas = {
     1: {
